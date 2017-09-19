52e9facf730944538220df5a9200539de40ca576
dessyamaliyaa <32087595+dessyamaliyaa@users.noreply.github.com>
2017-09-19 14:38:31 +0700

    Add files via upload

---
+++ DemoAritmatika.java
@@ -0,0 +1,47 @@
+public class DemoAritmatika{
+
+	public static void main(String[] args){
+	
+	// sedikit angka
+	int i=37;
+	int j=42;
+	double x=27.475;
+	double y=7.22;
+	System.out.println("Nilai dari variabel..");
+	System.out.println("i = " + i);
+	System.out.println("j = " + j);
+	System.out.println("x = " + x);
+	System.out.println("y = " + y);
+
+	// Penjumlahan Angka
+	System.out.println("Adding...");
+	System.out.println("i + j = " + (i + j));
+	System.out.println("x + y = " + (x + y));
+	
+	// Pengurangan Angka
+	System.out.println("Subtracting..");
+	System.out.println("i - j = " + (i - j));
+	System.out.println("x - y = " + (x - y));
+	
+	// Perkalian Angka
+	System.out.println("Multiplying..");
+	System.out.println("i * j = " + (i * j));
+	System.out.println("x * y = " + (x * y));
+
+	// Pembagian Angka
+	System.out.println("Dividing..");
+	System.out.println("i / j = " + (i / j));
+	System.out.println("x / y = " + (x / y));
+
+	// menghitung nilai modulus dari pembagian
+	System.out.println("Computing the remainder...");
+	System.out.println("i % j = " + (i % j));
+	System.out.println("x % y = " + (x % y));
+
+	// Tipe Penggabungan
+	System.out.println("Mixing types...");
+	System.out.println("j + y = " + (j + y));
+	System.out.println("i * x = " + (i * x));
+
+	}
+}
